3834222aa4a234b91124429e91ecf6c722784343
Rodrigo Favela <rodfb95@protonmail.com>
2026-05-20 12:35:46 -0600

    Add INSERT INTO tables values

--- backend/db/init.js
+++ backend/db/init.js
@@ -38,7 +38,36 @@ const createTables = async () => {
             date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
             user_id INTEGER REFERENCES users(id) ON DELETE CASCADE
              )`)
+        
+        await db.query(`
+            INSERT INTO users (name, lastname, password) 
+            VALUES 
+            ('Rodrigo', 'Favela', '$2a$12$DCuOk83./bZV5QnUXnBSW.APK8Gm025wQ9UDNc0XceqzwzKrVmu/2'), 
+            ('Julia', 'Bardales', '$2a$12$DCuOk83./bZV5QnUXnBSW.APK8Gm025wQ9UDNc0XceqzwzKrVmu/2'),
+            ('Veronica', 'Bardales', '$2a$12$DCuOk83./bZV5QnUXnBSW.APK8Gm025wQ9UDNc0XceqzwzKrVmu/2'),
+            ('Luis', 'Favela', '$2a$12$DCuOk83./bZV5QnUXnBSW.APK8Gm025wQ9UDNc0XceqzwzKrVmu/2');
+            `)
+        
+        await db.query(`
+            INSERT INTO doctors_data (doctor_name, doctor_contact) 
+            VALUES 
+            ('Roberto Quiroz', '555-1234'), 
+            ('Claudia Espinoza', '555-5678');
+            `)
+        
+        await db.query(`
+            INSERT INTO medicine (medicine, hour_to_take) 
+            VALUES 
+            ('Treda', '100 gr a las 2:00 pm'), 
+            ('Ibuprofeno', '100 mg 12:00 pm');
+            `)
 
+        await db.query(`
+            INSERT INTO exercises (exercise, time_sets) 
+            VALUES 
+            ('Caminar', '10 minutos'), 
+            ('Sentadillas', '20 minutes);
+            `)
 
         } catch (error) {
             console.error("Error at creating tables", error)
