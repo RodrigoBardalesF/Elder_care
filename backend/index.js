a04f925aa20d1b1327cfd7597a29e2cf792b3dd7
Rodrigo Favela <rodfb95@protonmail.com>
2026-05-19 23:43:05 -0600

    Comment out the console.log from index.js for added security to the data

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ app.get("/", async(req, res) => {
     try {
     const result = await db.query("SELECT * FROM medicine");
     const data = result.rows;
-    console.log(data)
+    //console.log(data)
     } catch (error){
           console.error("Error at GET /api/medicine:", error);
         res.status(500).json({error:"Error al mandar datos de medicina", error})}
@@ -50,7 +50,7 @@ app.get("/api/medicine", async(req, res) => {
 app.post("/api/medicine", async(req, res) => {
     try
     {const data = req.body
-    console.log(data)
+    //console.log(data)
     const result = await db.query("INSERT INTO medicine (medicine, hour_to_take) VALUES ($1, $2) RETURNING *", [data.medicine, data.hour_to_take]);
     res.status(200).json(result.rows[0])
     } catch (error) {
@@ -62,7 +62,7 @@ app.post("/api/medicine", async(req, res) => {
 app.put("/api/medicine/:id", async (req, res) => {
     try {
     const data = req.body
-    console.log(data);
+    //console.log(data);
 
     const {id} = req.params
     const result = await db.query("UPDATE medicine SET medicine = ($1), hour_to_take = ($2) WHERE id = ($3)", [data.medicine, data.hour_to_take, id])
@@ -96,7 +96,7 @@ app.get("/api/exercise", authenticateToken,async(req, res) => {
 app.post("/api/exercise", async(req, res) => {
     try
     {const data = req.body
-    console.log(data)
+    //console.log(data)
     const result = await db.query("INSERT INTO exercises (exercise, time_sets) VALUES ($1, $2) RETURNING *", [data.exercise, data.time_sets]);
     res.status(200).json(result.rows[0])
     } catch(error){
@@ -108,12 +108,12 @@ app.post("/api/exercise", async(req, res) => {
 app.put("/api/exercise/:id", async (req, res) => {
     try {
     const data = req.body
-    console.log(data);
+    //console.log(data);
     const {id} = req.params
     const result = await db.query("UPDATE exercises SET exercise = ($1), time_sets = ($2) WHERE id = ($3)", [data.exercise, data.time_sets, id])
     } catch (error) {
     console.error("Error at posting data from post / api/exercise")
-    res.status(500).json({error:"Error at posting medicine data", error})
+    res.status(500).json({error:"Error at posting exercise data", error})
     }
 })
 
@@ -141,7 +141,7 @@ app.get("/api/doctor_data", authenticateToken, async(req, res) => {
 app.post("/api/doctor_data", async(req, res) => {
     try
     {const data = req.body
-    console.log(data)
+    //console.log(data)
     const result = await db.query("INSERT INTO doctors_data (doctor_name, doctor_contact) VALUES ($1, $2) RETURNING *", [data.doctor_name, data.doctor_contact]);
     res.status(200).json(result.rows[0])
     } catch (error) {
@@ -153,12 +153,12 @@ app.post("/api/doctor_data", async(req, res) => {
 app.put("/api/doctor_data/:id", async (req, res) => {
     try {
     const data = req.body
-    console.log(data);
+    //console.log(data);
     const {id} = req.params
     const result = await db.query("UPDATE doctors_data SET doctor_name = ($1), doctor_contact = ($2) WHERE id = ($3)", [data.doctor_name, data.doctor_contact, id])
     } catch (error) {
-    console.error("Error at posting data from post / api/exercise")
-    res.status(500).json({error:"Error at posting medicine data", error})
+    console.error("Error at posting data from post / api/doctor_data")
+    res.status(500).json({error:"Error at posting doctor's data", error})
     }
 })
 
@@ -167,8 +167,8 @@ app.delete("/api/doctor_data/:id", authenticateToken, async(req, res) => {
     {const { id } = req.params
     const result = await db.query("DELETE FROM doctors_data WHERE id=($1)", [id]);
     } catch (error) {
-    console.error("Error at deleting data from delete/ api/exercise")
-    res.status(500).json({error:"Error at deleting docto's data", error})
+    console.error("Error at deleting data from delete/ api/doctor_data")
+    res.status(500).json({error:"Error at deleting doctor's data", error})
     }
 })
 
@@ -186,8 +186,8 @@ app.get("/api/notes", authenticateToken, async(req, res) => {
 app.post("/api/notes", authenticateToken, async(req, res) => {
     try
     {const data = req.body
-    console.log(req.user);
-    console.log(data)
+    //console.log(req.user);
+    //console.log(data)
     const result = await db.query("INSERT INTO notes (text, date, user_id) VALUES ($1, NOW(), $2) RETURNING *", [data.text, req.user.id]);
     res.status(200).json(result.rows[0])
     } catch (error) {
@@ -200,12 +200,12 @@ app.post("/api/notes", authenticateToken, async(req, res) => {
 app.put("/api/notes/:id", async (req, res) => {
     try {
     const data = req.body
-    console.log(data);
+    //console.log(data);
     const {id} = req.params
     const result = await db.query("UPDATE notes SET text = ($1) WHERE id = ($2);", [data.text, id])
     } catch (error) {
-    console.error("Error at posting data from post / api/exercise")
-    res.status(500).json({error:"Error at posting medicine data", error})
+    console.error("Error at posting data from post / api/notes")
+    res.status(500).json({error:"Error at posting note's data", error})
     }
 })
 
@@ -221,17 +221,17 @@ app.delete("/api/notes/:id", authenticateToken, async(req, res) => {
 
 app.post("/login", async (req, res) => {
     const {username, password} = req.body    
-    console.log(username);
-    console.log(password);
+    //console.log(username);
+    //console.log(password);
     try {                          
         const result = await db.query("SELECT * FROM users WHERE name=($1)", [username])
         const data = result.rows[0]
-        console.log("The data we receive from the DB is:", data)
+        //console.log("The data we receive from the DB is:", data)
             if (result.rows.length === 0) throw new Error ("Username doesn't exist") 
             const isValid = await bcrypt.compare(password, data.password)
             if (!isValid) throw new Error ("password is invalid")
             const token = jwt.sign({id: data.id}, process.env.TOKEN_SECRET, {expiresIn: '1h'})
-            console.log("The token we produce and send is:", token)
+            //console.log("The token we produce and send is:", token)
             res.json({token})
     } catch (error) {res.status(401).send(error.message)}
 });
